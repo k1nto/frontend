68e7ffb24e5b89a7f4c6d14693c08c9e81414b22
k1nto <bratukin2003@gmail.com>
2022-10-25 23:41:22 +0300

    Task 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
 function App() {
-  const handleClick = () => alert('vsem ku')
+
+  const handleClick = (e) => {
+    const log = e.target.login.value;
+    const pass = e.target.password.value;
+    // eslint-disable-next-line no-useless-concat
+    alert("Your login: " +log + "\n" + "Your password: " +pass)
+  }
   return (
-    <div className="container">
-      <input></input>
+    <div align="center" justify="center"className="container">
+      <form onSubmit={handleClick}>
+      Login:
+      <input type="text" name="login" placeholder="Login"></input>
+      <p>Password: 
+        <input type="text" name="password" placeholder="Password"></input>
+      </p>
+      <p>'
+        <button onClick={handleClick}>Log in</button>
+      </p>
+      </form>
     </div>
   );
 }
